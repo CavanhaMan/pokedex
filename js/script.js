ab172f99655c7a2df43a15814a4f1609b85215b6
Rodrigo Cavanha <cavanhaman@hotmail.com>
2022-08-01 01:06:05 -0300

    acesso a API retornando dados json

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+//fazendo acesso assincrono a API (usando await para aguardar a resposta)
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     console.log(APIResponse);
+
+    //acessando os dados json da consulta API:
+    const data = await APIResponse.json();
+    console.log(data);
+  
 }
 
 fetchPokemon('25');

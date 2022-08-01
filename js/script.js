5fa8996266972fb1a9700c162148a1b0dd0eba8e
Rodrigo Cavanha <cavanhaman@hotmail.com>
2022-08-01 01:02:05 -0300

    acesso inicial a API 200 ok

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    console.log(APIResponse);
+}
+
+fetchPokemon('25');
